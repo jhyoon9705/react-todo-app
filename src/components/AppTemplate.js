9c69cddec42c41cff6bc747828c6265ade68db14
jhyoon9705 <jhyoon513@naver.com>
2021-07-30 17:14:35 +0900

    build overall templates

---
+++ src/components/AppTemplate.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AppTemplate.scss';
+
+const AppTemplate = ({ children }) => {
+  return (
+    <div className="AppTemplate">
+      <div className="app-title">To-Do List</div>
+      <div className="contents">{children}</div>
+    </div>
+  );
+};
+
+export default AppTemplate;
